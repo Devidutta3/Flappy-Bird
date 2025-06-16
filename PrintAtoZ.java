ccd0e85a92c6772c1bdb4bf2b9737abc6302abc3
Devidutta Das <dasdevidutta3@gmail.com>
2025-06-16 21:52:28 +0530

    Create a PrintAtoZ.java
    Create a java Beginner Project. It is very simple.

---
+++ PrintAtoZ.java
@@ -0,0 +1,13 @@
+// import java.lang.*;
+class AtoZ {
+
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // char ch=sc.next().charAt(0);
+        System.out.print("Printing A to Z: ");
+        for (char i = 'A'; i <= 'Z'; i++) {
+            System.out.print(i+" ");
+        }
+
+    }
+}
